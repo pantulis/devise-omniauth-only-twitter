de09709371367b966fa6bbbbefab6dc3a39cb5b2
Juan Lupion <juan.lupion@the-cocktail.com>
2011-08-19 00:02:28 +0200

    Checkng the mockable attributes from Omniauth

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,13 +7,14 @@ class User < ActiveRecord::Base
   # Setup accessible (or protected) attributes for your model
   attr_accessible :nickname, :name, :remember_me
   
+  
   def self.find_for_twitter_oauth(omniauth)
     authentication = Authentication.find_by_provider_and_uuid(omniauth['provider'], omniauth['uid'])
     if authentication && authentication.user
       authentication.user
     else
-      user = User.create!(:nickname => omniauth['user_info']['nickname'], 
-                            :name => omniauth['user_info']['name'])
+      user = User.create!(:nickname => omniauth['nickname'], 
+                            :name => omniauth['name'])
       user.authentications.create!(:provider => omniauth['provider'], :uuid => omniauth['uid'])
       user.save
       user
